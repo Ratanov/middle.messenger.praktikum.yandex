1516ad26b8513ae7b57636b27b3d6399f54b640b
Oleg_Ratanov <o.ratanov@yandex.ru>
2024-06-13 21:28:04 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,21 @@ module.exports = {
     node: true,
     mocha: true,
   },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    camelcase: "error",
+    "spaced-comment": "error",
+    quotes: ["warn", "single"],
+    "@typescript-eslint/prefer-ts-expect-error": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prettier/prettier": "error"
+  },
   globals: {
     process: 'readonly',
     global: 'readonly',
@@ -12,28 +27,9 @@ module.exports = {
     document: 'readonly',
     Node: 'readonly',
     MouseEvent: 'readonly',
+    describe: 'readonly',
+    it: 'readonly',
+    before: 'readonly',
+    after: 'readonly',
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    "ecmaVersion": 12, // версия стандарта JavaScript. Последний 12 (2021).
-    "sourceType": "module" // Позволяет использовать import/export
-  },
-  plugins: ["@typescript-eslint", "prettier"],
-  rules: {
-    "camelcase": "error",
-    "spaced-comment": "error",
-    "quotes": ["warn", "single"],
-    "@typescript-eslint/prefer-ts-expect-error": "error",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "prettier/prettier": "error"
-  },
-  globals: {
-    "process": "readonly",
-    "global": "readonly",
-    "window": "readonly",
-    "document": "readonly",
-    "Node": "readonly",
-    "MouseEvent": "readonly"
-  }
 };
--- mochaTest.js
+++ mochaTest.js
@@ -4,7 +4,11 @@ const jsdom = new JSDOM('<div id="app"></div>', {
   url: 'http://localhost:3000',
 });
 
+// eslint-disable-next-line no-undef
 global.window = jsdom.window;
+// eslint-disable-next-line no-undef
 global.document = jsdom.window.document;
+// eslint-disable-next-line no-undef
 global.Node = jsdom.window.Node;
+// eslint-disable-next-line no-undef
 global.MouseEvent = jsdom.window.MouseEvent;
