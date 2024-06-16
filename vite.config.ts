f82980bddfdac3843039dab9034352714a961ac0
Oleg Ratanov <ratanov.pro@gmail.com>
2024-06-16 16:16:19 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
-import path from 'path';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
+  root: '.',
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'index.html'),
-      name: 'sprint_4',
-      fileName: (format) => `sprint4.${format}.js`,
-    },
     outDir: 'dist',
   },
-  root: '.',
   plugins: [
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
